9ad1dd6d594f377eae75d203f437273341066b33
SinaC <heymbeeck.joel@gmail.com>
2025-12-26 00:42:45 +0100

    #206 : namespaces fixed

--- Mud.Common/Attributes/ExportAttribute.cs
+++ Mud.Common/Attributes/ExportAttribute.cs
@@ -1,28 +1,27 @@
-﻿namespace Mud.Common.Attributes
+﻿namespace Mud.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class ExportAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
-    public class ExportAttribute : Attribute
+    public ExportAttribute() : this(null, null)
     {
-        public ExportAttribute() : this(null, null)
-        {
-        }
-
-        public ExportAttribute(Type contractType) : this(null, contractType)
-        {
-        }
-
-        public ExportAttribute(string contractName) : this(contractName, null)
-        {
-        }
-
-        public ExportAttribute(string? contractName, Type? contractType)
-        {
-            ContractName = contractName;
-            ContractType = contractType;
-        }
-
-        public string? ContractName { get; }
-
-        public Type? ContractType { get; }
     }
+
+    public ExportAttribute(Type contractType) : this(null, contractType)
+    {
+    }
+
+    public ExportAttribute(string contractName) : this(contractName, null)
+    {
+    }
+
+    public ExportAttribute(string? contractName, Type? contractType)
+    {
+        ContractName = contractName;
+        ContractType = contractType;
+    }
+
+    public string? ContractName { get; }
+
+    public Type? ContractType { get; }
 }
--- Mud.Common/Attributes/ExportInspector.cs
+++ Mud.Common/Attributes/ExportInspector.cs
@@ -1,69 +1,67 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Reflection;
 
-namespace Mud.Common.Attributes
+namespace Mud.Common.Attributes;
+
+public static class ExportInspector
 {
-    public static class ExportInspector
+    public static void Register(IServiceCollection services, Type type)
     {
-        public static void Register(IServiceCollection services, Type type)
+        ArgumentNullException.ThrowIfNull(type);
+
+        if (!type.CustomAttributes.Any())
         {
-            ArgumentNullException.ThrowIfNull(type);
+            return;
+        }
 
-            if (!type.CustomAttributes.Any())
+        var exports = type.GetCustomAttributes<ExportAttribute>().ToArray();
+
+        if (exports.Length == 0)
+        {
+            return;
+        }
+
+        var isSingleton = type.CustomAttributes.Any(a => a.AttributeType == typeof(SharedAttribute));
+
+        if (isSingleton && exports.Length > 1)
+        {
+            if (exports.Any(x => x.ContractType is null || x.ContractName is not null))
+                throw new InvalidOperationException($"Exported type {type.FullName} is declared as Shared with multiple exports. ContractType is mandatory and ContractName is forbidden for each Export");
+
+            services.AddSingleton(type);
+            foreach (var export in exports)
             {
-                return;
+                services.AddSingleton(export.ContractType!, x => x.GetRequiredService(type));
             }
-
-            var exports = type.GetCustomAttributes<ExportAttribute>().ToArray();
-
-            if (exports.Length == 0)
+        }
+        else
+        {
+            foreach (var export in exports)
             {
-                return;
-            }
+                var fromType = export.ContractType ?? type;
+                var contractName = export.ContractName;
 
-            var isSingleton = type.CustomAttributes.Any(a => a.AttributeType == typeof(SharedAttribute));
+                if (!fromType.IsAssignableFrom(type))
+                    throw new InvalidOperationException($"Exported type {type.FullName} does not implement the contract type {fromType.FullName}.");
 
-            if (isSingleton && exports.Length > 1)
-            {
-                if (exports.Any(x => x.ContractType is null || x.ContractName is not null))
-                    throw new InvalidOperationException($"Exported type {type.FullName} is declared as Shared with multiple exports. ContractType is mandatory and ContractName is forbidden for each Export");
-
-                services.AddSingleton(type);
-                foreach (var export in exports)
+                if (isSingleton)
                 {
-                    services.AddSingleton(export.ContractType!, x => x.GetRequiredService(type));
-                }
-            }
-            else
-            {
-                foreach (var export in exports)
-                {
-                    var fromType = export.ContractType ?? type;
-                    var contractName = export.ContractName;
-
-                    if (!fromType.IsAssignableFrom(type))
-                        throw new InvalidOperationException($"Exported type {type.FullName} does not implement the contract type {fromType.FullName}.");
-
-                    if (isSingleton)
+                    if (string.IsNullOrWhiteSpace(contractName))
                     {
-                        if (string.IsNullOrWhiteSpace(contractName))
-                        {
-                            services.AddSingleton(fromType, type);
-                        }
-                        else
-                        {
-                            services.AddKeyedSingleton(fromType, contractName!, type);
-                        }
-                    }
-                    else if (string.IsNullOrWhiteSpace(contractName))
-                    {
-                        services.AddTransient(fromType, type);
+                        services.AddSingleton(fromType, type);
                     }
                     else
                     {
-                        services.AddKeyedTransient(fromType, contractName!, type);
+                        services.AddKeyedSingleton(fromType, contractName!, type);
                     }
+                }
+                else if (string.IsNullOrWhiteSpace(contractName))
+                {
+                    services.AddTransient(fromType, type);
+                }
+                else
+                {
+                    services.AddKeyedTransient(fromType, contractName!, type);
                 }
             }
         }
