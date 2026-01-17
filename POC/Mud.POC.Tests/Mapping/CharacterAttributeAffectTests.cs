93efe366f50c08806d1e01ffa0632d302dea8499
SinaC <heymbeeck.joel@gmail.com>
2026-01-17 10:54:16 +0100

    WIP

--- POC/Mud.POC.Tests/Mapping/CharacterAttributeAffectTests.cs
+++ POC/Mud.POC.Tests/Mapping/CharacterAttributeAffectTests.cs
@@ -3,44 +3,44 @@ using Mud.Server.Affects.Character;
 using Mud.Server.Domain;
 using Riok.Mapperly.Abstractions;
 
-namespace Mud.POC.Tests.Mapping
+namespace Mud.POC.Tests.Mapping;
+
+[TestClass]
+public class CharacterAttributeAffectTests
 {
-    [TestClass]
-    public class CharacterAttributeAffectTests
+    [TestMethod]
+    public void CharacterAttributeAffect_CharacterAttributeAffectData()
     {
-        [TestMethod]
-        public void CharacterAttributeAffect_CharacterAttributeAffectData()
-        {
-            var characterAttributeAffect = new CharacterAttributeAffect { Location = CharacterAttributeAffectLocations.ArmorPierce, Modifier = 15, Operator = AffectOperators.Assign };
+        var characterAttributeAffect = new CharacterAttributeAffect { Location = CharacterAttributeAffectLocations.ArmorPierce, Modifier = 15, Operator = AffectOperators.Assign };
 
-            var mapper = new CharacterAttributeAffectMapper();
-            var characterAttributeAffectData = mapper.CharacterAttributeAffectToCharacterAttributeAffectData(characterAttributeAffect);
+        var mapper = new CharacterAttributeAffectMapper();
+        var characterAttributeAffectData = mapper.CharacterAttributeAffectToCharacterAttributeAffectData(characterAttributeAffect);
 
-            Assert.IsNotNull(characterAttributeAffectData);
-            Assert.AreEqual(CharacterAttributeAffectLocations.ArmorPierce, characterAttributeAffectData.Location);
-            Assert.AreEqual(15, characterAttributeAffectData.Modifier);
-            Assert.AreEqual(AffectOperators.Assign, characterAttributeAffectData.Operator);
-        }
-
-        [TestMethod]
-        public void CharacterAttributeAffectData_CharacterAttributeAffect()
-        {
-            var characterAttributeAffectData = new CharacterAttributeAffectData { Location = CharacterAttributeAffectLocations.ArmorPierce, Modifier = 15, Operator = AffectOperators.Assign };
-
-            var mapper = new CharacterAttributeAffectMapper();
-            var characterAttributeAffect = mapper.CharacterAttributeAffectDataToCharacterAttributeAffect(characterAttributeAffectData);
-
-            Assert.IsNotNull(characterAttributeAffect);
-            Assert.AreEqual(CharacterAttributeAffectLocations.ArmorPierce, characterAttributeAffect.Location);
-            Assert.AreEqual(15, characterAttributeAffect.Modifier);
-            Assert.AreEqual(AffectOperators.Assign, characterAttributeAffect.Operator);
-        }
+        Assert.IsNotNull(characterAttributeAffectData);
+        Assert.AreEqual(CharacterAttributeAffectLocations.ArmorPierce, characterAttributeAffectData.Location);
+        Assert.AreEqual(15, characterAttributeAffectData.Modifier);
+        Assert.AreEqual(AffectOperators.Assign, characterAttributeAffectData.Operator);
     }
 
-    [Mapper]
-    public partial class CharacterAttributeAffectMapper
+    [TestMethod]
+    public void CharacterAttributeAffectData_CharacterAttributeAffect()
     {
-        public partial CharacterAttributeAffectData CharacterAttributeAffectToCharacterAttributeAffectData(CharacterAttributeAffect characterAttributeAffect);
-        public partial CharacterAttributeAffect CharacterAttributeAffectDataToCharacterAttributeAffect( CharacterAttributeAffectData characterAttributeAffectData);
+        var characterAttributeAffectData = new CharacterAttributeAffectData { Location = CharacterAttributeAffectLocations.ArmorPierce, Modifier = 15, Operator = AffectOperators.Assign };
+
+        var mapper = new CharacterAttributeAffectMapper();
+        var characterAttributeAffect = mapper.CharacterAttributeAffectDataToCharacterAttributeAffect(characterAttributeAffectData);
+
+        Assert.IsNotNull(characterAttributeAffect);
+        Assert.AreEqual(CharacterAttributeAffectLocations.ArmorPierce, characterAttributeAffect.Location);
+        Assert.AreEqual(15, characterAttributeAffect.Modifier);
+        Assert.AreEqual(AffectOperators.Assign, characterAttributeAffect.Operator);
     }
+}
+
+
+[Mapper]
+public partial class CharacterAttributeAffectMapper
+{
+    public partial CharacterAttributeAffectData CharacterAttributeAffectToCharacterAttributeAffectData(CharacterAttributeAffect characterAttributeAffect);
+    public partial CharacterAttributeAffect CharacterAttributeAffectDataToCharacterAttributeAffect(CharacterAttributeAffectData characterAttributeAffectData);
 }
